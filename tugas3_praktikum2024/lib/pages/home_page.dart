2a6988b54fa83f55401fefa0df6fbd7fd11bd996
Keenan <keenanadityo167@gmail.com>
2024-10-02 11:48:30 +0700

    finish project

--- tugas3_praktikum2024/lib/pages/home_page.dart
+++ tugas3_praktikum2024/lib/pages/home_page.dart
@@ -1,8 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tugas3_praktikum2024/pages/login_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String _username = "";
+  @override
+  void initState() {
+    super.initState();
+    _getUsername();
+  }
+
+  Future<void> _getUsername() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String username = prefs.getString("username") ?? "Anonymous";
+
+    setState(() {
+      _username = username;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,8 +36,8 @@ class HomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("Hai Keenan"),
-            Text("Passwordmu ini kan"),
+            Text("Hai $_username"),
+            Text("Selamat datang!"),
           ],
         ),
       ),
--- tugas3_praktikum2024/lib/pages/login_page.dart
+++ tugas3_praktikum2024/lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tugas3_praktikum2024/pages/home_page.dart';
 
 class LoginPage extends StatefulWidget {
@@ -9,6 +10,9 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,29 +25,58 @@ class _LoginPageState extends State<LoginPage> {
           ),
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(), label: Text("Username")),
-            ),
+            child: _showInput(_usernameController, "Username", false),
           ),
           Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(), label: Text("Password")),
-            ),
-          ),
+              padding: const EdgeInsets.all(8.0),
+              child: _showInput(_passwordController, "Password", true)),
           ElevatedButton(
               onPressed: () {
-                Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => HomePage(),
-                    ));
+                if (_passwordController.text == "admin") {
+                  if (_usernameController.text.isEmpty) {
+                    _showDiaglog("usernamenya jangan dikosongin kak");
+                  } else {
+                    _saveUsername();
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => HomePage(),
+                        ));
+                  }
+                } else {
+                  _showDiaglog("Salah kak, passnya harus \"admin\"");
+                }
               },
               child: Text("Masuk"))
         ],
       ),
+    );
+  }
+
+  void _saveUsername() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString("username", _usernameController.text);
+  }
+
+  TextField _showInput(controller, label, isObscure) {
+    return TextField(
+      controller: controller,
+      obscureText: isObscure,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        label: Text(label),
+      ),
+    );
+  }
+
+  _showDiaglog(message) {
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text(message),
+        );
+      },
     );
   }
 }
