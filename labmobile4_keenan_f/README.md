ffef9491ec119994ef97a71739e9007f685224cc
Keenan <keenanadityo167@gmail.com>
2024-10-03 22:14:45 +0700

    add task 4

---
+++ labmobile4_keenan_f/README.md
@@ -0,0 +1,3 @@
+# labmobile4_keenan_f
+
+A new Flutter project.
---
+++ labmobile4_keenan_f/android/app/src/main/kotlin/com/example/labmobile4_keenan_f/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.labmobile4_keenan_f
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
