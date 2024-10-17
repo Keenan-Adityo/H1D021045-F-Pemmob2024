334f469cf889d48fa7172c74a8a0ff52589b704e
Keenan <keenanadityo167@gmail.com>
2024-10-17 10:26:52 +0700

    pertemuan 4

--- labmobile4_keenan_f/lib/main.dart
+++ labmobile4_keenan_f/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:labmobile4_keenan_f/ui/login_page.dart';
+import 'package:labmobile4_keenan_f/ui/produk_page.dart';
+import 'package:labmobile4_keenan_f/ui/registrasi_page.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +12,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return const MaterialApp(home: LoginPage());
   }
 }
