4e14df0cecc4d7c3ca1032562c982a3c1b9334dd
Keenan <keenanadityo167@gmail.com>
2024-10-31 23:17:23 +0700

    tugas 6

---
+++ LabMobile6_Keenan_F/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'LabMobile6_Keenan_F',
+  webDir: 'www'
+};
+
+export default config;
